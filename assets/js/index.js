eaab272f3511016a8625f0c40b363fc25a447325
Gustavo.atton <gustavo.atton04@gmail.com>
2025-05-20 18:20:29 -0400

    modificacion js y html

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,27 @@
-let precio = 400000
+let preciobase = 400000;
+let cantidad = 0 ;
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio
+let precioSpan = document.querySelector('.precio-inicial');
+precioSpan.innerHTML = preciobase;
 
-let btnmas = documento.querySelector (".btnmas"); 
+const btnmas = document.querySelector('.btnmas');
+const btnmenos = document.querySelector('.btnmenos');
+
+let cantidadSpan = document.querySelector('.cantidad');
+let total = document.querySelector('.valor-total');
+
+
+
+function restar() {
+  if (cantidad > 0) {
+    cantidad--;
+    cantidadSpan.innerHTML = cantidad;
+    total.innerHTML = cantidad * preciobase;
+  }
+}
+
+function sumar() {
+  cantidad++;
+  cantidadSpan.innerHTML = cantidad;
+  total.innerHTML = cantidad * preciobase;
+} 
